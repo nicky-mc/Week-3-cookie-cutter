f01eac323d98097d89273b8583246a10d8d96429
nicky-mc <cinimod@gmail.com>
2024-09-12 16:08:23 +0100

    some framework for javascript and gif for button

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 console.log("do you like cookies?");
+// we need two global values :cookie count and cookies per second
+let cookieCount = 0;
+let cookiesPerSecond = 0;
+
+let stats = {
+    cookieCount: 0;
+    cookiesPerSecond: 0;
+};
+// Dom Manipulation - selct the DOM elements (buttons imgs p etc
+
+
+// we need a way to store the shope upgrades that come from the API 
+let shopUpgrades = []
+
+function getShopUpgrades() {
+    //fetch the data
+    // translate into JSON
+    //Push the items into the shopUpgrades array above
+}
+// an event listener to click on my cookie
+//select the cookie or button
+function handleCookieCLick() {
+    // logic should ensure the cookie count increases by one
+}
+addEventListener("click", handleCookieCLick);
+
+// we need a timer that decreases the cookies per second value by 1 per second
+setInterval(functiuon() {
+    //i want to increase the value of cookieCOunt by one every second
+}, 1000);
+
+function storeLocalStorage() {
+    // a method that stringifies your data : these blocks are guidance to any issues 
+}
+
